027857914bf27d79cebb0df3ded52966e227ac30
覃国强 <qinguoqiang@xiaomi.com>
2020-12-15 19:52:32 +0800

    Feature: git auto push

---
+++ src/main/java/test/java/lang/IntegerTest.java
@@ -0,0 +1,25 @@
+package test.java.lang;
+
+/**
+ * @author 覃国强
+ * @date 2020-12-15 19:35
+ */
+public class IntegerTest {
+
+  public static void main(String[] args) {
+    testNumberOfLeadingZeros();
+
+    // 111 1111  1111 1111  1111 1111  1111 1111
+    String s = Integer.toBinaryString(0x7fffffff);
+    System.out.println(s);
+  }
+
+
+  public static void testNumberOfLeadingZeros() {
+    System.out.println(Integer.numberOfLeadingZeros(0));
+    System.out.println(Integer.numberOfLeadingZeros(1));
+    System.out.println(Integer.numberOfLeadingZeros(1 << 16));
+    System.out.println(Integer.numberOfLeadingZeros(1 << 30));
+  }
+
+}
---
+++ src/main/java/test/java/util/concurrent/ConcurrentHashMapTest.java
@@ -0,0 +1,15 @@
+package test.java.util.concurrent;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author 覃国强
+ * @date 2020-12-15 19:37
+ */
+public class ConcurrentHashMapTest {
+
+  public static void main(String[] args) {
+    ConcurrentHashMap<String, Object> map = new ConcurrentHashMap<>();
+  }
+
+}
